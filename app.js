8e8f051ffc582b32d2b3c4b171278c51ed650c6b
Ben Frenkel <ben@concussionjs.com>
2016-12-29 04:30:52 -0800

    updated app.js again

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Gigster! Great to meet you. You are the best!!')
+  res.send('Hello Gigster the Great! Great to meet you. You are the best!!')
 })
 
 var server = app.listen(3000, function () {
