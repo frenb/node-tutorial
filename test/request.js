74952d897508cd19b4501cf8f128bb022d005ea8
Ben Frenkel <ben@concussionjs.com>
2016-12-28 16:34:11 -0800

    moved test/spec.js to test/request.js

--- test/request.js
+++ test/request.js
@@ -1,20 +1,21 @@
-var request = require('supertest');
+var request = require('request');
+var assert = require('assert');
+var base_url = "http://localhost:3000/";
+
 describe('loading express', function () {
-  var server;
-  beforeEach(function () {
-    server = require('./../app');
-  });
-  afterEach(function () {
-    server.close();
-  });
-  it('responds to /', function testSlash(done) {
-  request(server)
-    .get('/')
-    .expect(200, done);
-  });
-  it('404 everything else', function testPath(done) {
-    request(server)
-      .get('/foo/bar')
-      .expect(404, done);
+  describe("GET /", function() {
+    it('responds to /', function testSlash(done) {
+      request.get(base_url, function(error, response, body) {
+        assert.equal(200, response.statusCode);
+        done();
+      });
+     });
+
+    it('404 everything else', function testPath(done) {
+      request.get(base_url+ "/foo/bar", function(error, response, body) {
+        assert.equal(404, response.statusCode);
+        done();
+      });
+    });
   });
 });
