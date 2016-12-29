e6e6bc111309ddba807d2869bcbd2acc6c260466
Ben Frenkel <ben@concussionjs.com>
2016-12-29 03:44:38 -0800

    updated return string in server

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Gigster!"')
+  res.send('Hello Gigster! great to meet you')
 })
 
 var server = app.listen(3000, function () {
