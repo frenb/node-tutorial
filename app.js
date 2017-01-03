e6a717d3a3214611dd399c6bbe7a32fa9d3ddd85
Ben Frenkel <ben@concussionjs.com>
2017-01-03 01:58:46 -0800

    updated response to Hello Gigsternano app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello great, grand world of Gigster!!')
+  res.send('Hello Gigster!!')
 })
 
 var server = app.listen(port, function () {
