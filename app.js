908e541b061cc16a2f57644924684573df43b3fb
Ben Frenkel <ben@concussionjs.com>
2017-01-04 03:02:23 -0800

    updated response

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello Gigster on heroku + kubernetes!! wow!! wee!! wowza!!')
+  res.send('Hello Gigster on heroku + kubernetes!! wow!! wee!! wowza!! zigzag!!')
 })
 
 var server = app.listen(port, function () {
